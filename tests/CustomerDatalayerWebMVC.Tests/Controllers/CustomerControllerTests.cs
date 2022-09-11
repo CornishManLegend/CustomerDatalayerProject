11a78714807f46f214e6ab3546bafa7c03274887
m.kalenishin <m.kalenishin@ylab.io>
2022-09-11 21:01:43 +0300

    fix: edited CustomerControllerTests

--- tests/CustomerDatalayerWebMVC.Tests/Controllers/CustomerControllerTests.cs
+++ tests/CustomerDatalayerWebMVC.Tests/Controllers/CustomerControllerTests.cs
@@ -10,6 +10,8 @@ using Moq;
 using PagedList;
 using Castle.Core.Resource;
 using CustomerDatalayer.Repositories;
+using System.Dynamic;
+using CustomerDatalayer.Services;
 
 namespace CustomerDatalayerWebMVC.Tests.Controllers
 {
@@ -26,35 +28,47 @@ namespace CustomerDatalayerWebMVC.Tests.Controllers
         }
 
 
-        //[TestMethod]
-        //public void ShouldReturnListOfCustomers()
-        //{
-        //    var controller = new CustomerController();
-        //    var customersResult = controller.Index(1);
-        //    var customersView = customersResult as ViewResult;
-        //    var customerModel = customersView.Model as PagedList<Customer>;
-        //    Assert.IsTrue(customerModel.Count != 0);
-        //}
-
-
-        //[TestMethod]
-        //public void ShouldBeAbleToCreateRedirect()
-        //{
-        //    var customerController = new CustomerController();
-        //    var result = customerController.Create(1, new Customer()
-        //    {
-        //        FirstName = "testJohn",
-        //        LastName = "testWayne",
-        //        PhoneNumber = "123456789111111",
-        //        Email = "johnWayne@gmail.com",
-        //        TotalPurchasesAmount = 1
-        //    }) as RedirectToRouteResult;
-        //    Assert.IsNotNull(result);
-        //}
+        [TestMethod]
+        public void ShouldBeAbleToCreateRedirect()
+        {
+            var customerController = new CustomerController();
+            var result = customerController.Create(1, new Customer()
+            {
+                FirstName = "testJohn",
+                LastName = "testWayne",
+                PhoneNumber = "123456789111111",
+                Email = "johnWayne@gmail.com",
+                TotalPurchasesAmount = 1
+            }) as RedirectToRouteResult;
+            Assert.IsNotNull(result);
+        }
 
 
         [TestMethod]
-        public void ShouldCreateCustomer()
+        public void ShouldBeAbleReturnListOfCustomers()
+        {
+            var controller = new CustomerController();
+            var customersResult = controller.Index(1);
+            var customersView = customersResult as ViewResult;
+            var customerModel = customersView.Model as PagedList<Customer>;
+            Assert.IsTrue(customerModel.Count != 0);
+        }
+
+
+
+        [TestMethod]
+        public void ShouldBeAbleToCallCustomerCreatePage()
+        {
+            var controller = new CustomerController();
+            var customersResult = controller.Create();
+            var customersView = customersResult as ViewResult;
+
+            Assert.IsNotNull(customersView);
+        }
+
+
+        [TestMethod]
+        public void ShouldBeAbleToCreateCustomer()
         {
             var customerControllerMock = new Mock<ICustomerService>();
             var customerController = new CustomerController(customerControllerMock.Object);
@@ -112,6 +126,30 @@ namespace CustomerDatalayerWebMVC.Tests.Controllers
         }
 
 
+        [TestMethod]
+        public void ShouldBeAbleToReturnCustomerDetails()
+        {
+            Customer customer = new Customer()
+            {
+                CustomerId = 1,
+                FirstName = "John",
+                LastName = "Wayne",
+                Email = "johnWayne@gmail.com",
+                PhoneNumber = "123456789111111",
+                TotalPurchasesAmount = 10
+            };
+
+            var customerControllerMock = new Mock<ICustomerService>();
+            customerControllerMock.Setup(x => x.Create(customer)).Returns(customer);
+            customerControllerMock.Setup(x => x.GetCustomer(customer.CustomerId)).Returns(customer);
+            var customerController = new CustomerController(customerControllerMock.Object);
+
+            var controller = new CustomerController(customerControllerMock.Object);
+            var result = controller.Details(customer.CustomerId);
+            var resultView = result as ViewResult;
+
+            Assert.IsNotNull(resultView);
+        }
 
     }
 }
