578e887b7639705da29685af2d9ab8fcc21583e1
m.kalenishin <m.kalenishin@ylab.io>
2022-09-11 11:31:08 +0300

    feat: added CustomerControllerTests

---
+++ tests/CustomerDatalayerWebMVC.Tests/Controllers/CustomerControllerTests.cs
@@ -0,0 +1,117 @@
+﻿using CustomerDatalayer.BusinessEntities;
+using CustomerDatalayer.Interfaces;
+using CustomerDatalayerWebMVC.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+using Moq;
+using PagedList;
+using Castle.Core.Resource;
+using CustomerDatalayer.Repositories;
+
+namespace CustomerDatalayerWebMVC.Tests.Controllers
+{
+
+    [TestClass]
+    public class CustomerControllerTests
+    {
+
+        [TestMethod]
+        public void ShouldBeAbleToCreateCustomerController()
+        {
+            var controller = new CustomerController();
+            Assert.IsNotNull(controller);
+        }
+
+
+        [TestMethod]
+        public void ShouldReturnListOfCustomers()
+        {
+            var controller = new CustomerController();
+            var customersResult = controller.Index(1);
+            var customersView = customersResult as ViewResult;
+            var customerModel = customersView.Model as PagedList<Customer>;
+            Assert.IsTrue(customerModel.Count != 0);
+        }
+
+
+        //[TestMethod]
+        //public void ShouldBeAbleToCreateRedirect()
+        //{
+        //    var customerController = new CustomerController();
+        //    var result = customerController.Create(1, new Customer()
+        //    {
+        //        FirstName = "testJohn",
+        //        LastName = "testWayne",
+        //        PhoneNumber = "123456789111111",
+        //        Email = "johnWayne@gmail.com",
+        //        TotalPurchasesAmount = 1
+        //    }) as RedirectToRouteResult;
+        //    Assert.IsNotNull(result);
+        //}
+
+
+        [TestMethod]
+        public void ShouldCreateCustomer()
+        {
+            var customerControllerMock = new Mock<ICustomerService>();
+            var customerController = new CustomerController(customerControllerMock.Object);
+            customerController.Create();
+            var customer = new Customer()
+            {
+                FirstName = "John",
+                LastName = "Wayne",
+                Email = "johnWayne@gmail.com",
+                PhoneNumber = "123456789111111",
+                TotalPurchasesAmount = 10
+            };
+            var result = customerController.Create(1, customer) as RedirectToRouteResult;
+            Assert.IsNotNull(result);
+            customerControllerMock.Verify(x => x.Create(customer));
+        }
+
+
+        [TestMethod]
+        public void ShouldBeAbleToEditCustomer()
+        {
+            var customerControllerMock = new Mock<ICustomerService>();
+            var customerController = new CustomerController(customerControllerMock.Object);
+            var customer = new Customer()
+            {
+                FirstName = "John",
+                LastName = "Wayne",
+                Email = "johnWayne@gmail.com",
+                PhoneNumber = "123456789111111",
+                TotalPurchasesAmount = 10
+            };
+            customerController.Edit(5);
+            var result = customerController.Edit(customer) as RedirectToRouteResult;
+            Assert.IsNotNull(result);
+            customerControllerMock.Verify(x => x.UpdateCustomer(customer));
+        }
+
+
+        [TestMethod]
+        public void ShouldBeAbleToDeleteCustomer()
+        {
+            var customerControllerMock = new Mock<ICustomerService>();
+            var customerController = new CustomerController(customerControllerMock.Object);
+            customerController.Delete(10);
+            var result = customerController.Delete(10, new Customer
+            {
+                FirstName = "John",
+                LastName = "Wayne",
+                PhoneNumber = "123456789111111",
+                Email = "johnWayne@gmail.com",
+                TotalPurchasesAmount = 10
+            }) as RedirectToRouteResult;
+            Assert.IsNotNull(result);
+            customerControllerMock.Verify(x => x.DeleteCustomer(10));
+        }
+
+
+
+    }
+}
---
+++ tests/CustomerDatalayerWebMVC.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomerDatalayerWebMVC.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomerDatalayerWebMVC.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("46e58e60-c6a7-4677-8839-60929dbd7dcc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
