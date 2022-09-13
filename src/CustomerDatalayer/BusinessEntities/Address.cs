30186393c0e96a8167fb83bdb95471dd816bce27
m.kalenishin <m.kalenishin@ylab.io>
2022-09-13 22:18:49 +0300

    feat: added validators to BusinessEntities

--- src/CustomerDatalayer/BusinessEntities/Address.cs
+++ src/CustomerDatalayer/BusinessEntities/Address.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,12 +17,17 @@ namespace CustomerDatalayer.BusinessEntities
         public int CustomerId { get; set; }
 
         [DisplayName("Address Line1")]
+        [Required]
+        [StringLength(100)]
         public string AddressLine1 { get; set; } = string.Empty;
 
         [DisplayName("Address Line2")]
+        [StringLength(100)]
         public string AddressLine2 { get; set; }
 
         [DisplayName("Address Type")]
+        [Required]
+        [RegularExpression(@"^(Shipping|Billing)$", ErrorMessage = "The Address Type should be Shipping or Billing")]
         public string AddressTypeAsString
         {
             get
@@ -37,15 +43,23 @@ namespace CustomerDatalayer.BusinessEntities
         public AddrTypes AddressType { get; set; }
 
         [DisplayName("City")]
+        [Required]
+        [StringLength(50)]
         public string City { get; set; } = string.Empty;
 
         [DisplayName("Postal Code")]
+        [Required]
+        [RegularExpression(@"^[0-9]{0,6}$", ErrorMessage = "The Postal Code cannot be longer than 6 characters. Only integers")]
         public string PostalCode { get; set; } = string.Empty;
 
         [DisplayName("State")]
+        [Required]
+        [StringLength(20)]
         public string AddrState { get; set; } = string.Empty;
 
         [DisplayName("Country")]
+        [Required]
+        [RegularExpression(@"^(USA|Canada)$", ErrorMessage = "The Country name can be either USA or Canada")]
         public string Country { get; set; } = string.Empty;
     }
 
--- src/CustomerDatalayer/BusinessEntities/Customer.cs
+++ src/CustomerDatalayer/BusinessEntities/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -14,20 +15,26 @@ namespace CustomerDatalayer.BusinessEntities
         public int CustomerId { get; set; }
 
         [DisplayName("First Name")]
+        [StringLength(50)]
         public string FirstName { get; set; }
 
         [DisplayName("Last Name")]
+        [Required]
+        [StringLength(50)]
         public string LastName { get; set; }
 
         [DisplayName("Phone Number")]
+        [RegularExpression(@"^[1-9]\d{14,14}$", ErrorMessage = "Incorrect the phone number format")]
         public string PhoneNumber { get; set; } = string.Empty;
 
         [DisplayName("Email")]
+        [RegularExpression(@"^[^\s@]+@[^\s@]+\.[^\s@]+$", ErrorMessage = "Incorrect the email format")]
         public string Email { get; set; } = string.Empty;
         public List<Address> Addresses { get; set; } = new List<Address>();
         public List<Note> Notes { get; set; } = new List<Note>();
 
         [DisplayName("Total Purchases Amount")]
+        [RegularExpression(@"^[\+]?[0-9]*(\.[0-9]+)?", ErrorMessage = "The value cannot be negative")]
         public decimal? TotalPurchasesAmount { get; set; } = 0;
     }
 }
